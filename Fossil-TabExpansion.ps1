d171a37cee2ef3624409e9900a29d06e6b609ff1
Angelo Scotto <scotto.a@gmail.com>
2014-03-24 19:23:38 +0100

    Initial commit of powershell scripts
    Actually only two files exists, a generic Fossil-Helpers and a custom
    TabExpansion file.

---
+++ Fossil-TabExpansion.ps1
@@ -0,0 +1,8 @@
+# Initial implementation by Angelo Simone Scotto
+# based on http://www.jeremyskinner.co.uk/2010/03/07/using-git-with-windows-powershell/
+
+$subcommands = @{
+    bisect = "bad good log chart next options reset status undo"
+    stash = "save snapshot list show pop apply goto drop diff gdiff"
+
+}
